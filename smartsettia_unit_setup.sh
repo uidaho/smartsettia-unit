e2242645c2966f396d23f61f83ca2d66b733c064
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-13 23:55:02 -0700

    Added sudo checker & ramdisk to setup script
    Asks for sudo if not sudo
    Installs & sets-up 10MB ramdisk for webcam storage

--- smartsettia_unit_setup.sh
+++ smartsettia_unit_setup.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+# get root permission
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 echo "Setting up smartsettia"
 
-sudo apt update
+apt update
 #https://www.saltycrane.com/blog/2010/02/how-install-pip-ubuntu/
-sudo apt install  python-pip
-sudo apt install --upgrade pip
-sudo apt install --upgrade vertualenv
+apt install  python-pip -y
+apt install --upgrade pip
+apt install --upgrade vertualenv
 
 
-echo "Installing dependencies"
-sudo pip install schedule
+echo -e "\nInstalling dependencies"
+pip install schedule
 
-echo "Done"
+echo -e "\nSetting up ramdisk for pictures (disabled atm)"
+apt install tmpfs -y    #install the ramdisk software
+mkdir -p /mnt/ramdisk   #make the mount directory
+MOUNTCODE="tmpfs       /mnt/ramdisk tmpfs   nodev,nosuid,noexec,nodiratime,size=11M   0 0"
+#check if fstab already has this line. if not add it.
+grep -q -F "$MOUNTCODE" /etc/fstab || echo "$MOUNTCODE" >> /etc/fstab
+mount -a               # mount the ramdisk
+
+echo -e "\nDone"
