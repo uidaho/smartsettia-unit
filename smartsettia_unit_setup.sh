51458162bfc6e10e768b89f9cecddccbe90e30c5
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-14 15:15:17 -0700

    Added install options to setup

--- smartsettia_unit_setup.sh
+++ smartsettia_unit_setup.sh
@@ -6,6 +6,27 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
+FLAG_GPIO = 0;
+FLAG_RAMDISK = 0;
+
+while true; do
+    read -p "Is this Device a Raspberri Pi? " yn
+    case $yn ins
+        [Yy]* ) FLAG_GPIO = 1; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wish to setup the ramdisk? " yn
+    case $yn in
+        [Yy]* ) FLAG_RAMDISK = 1; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 echo "Setting up smartsettia"
 
 apt update
@@ -18,12 +39,14 @@ apt install --upgrade vertualenv
 echo -e "\nInstalling dependencies"
 pip install schedule
 
-echo -e "\nSetting up ramdisk for pictures"
-apt install tmpfs -y    #install the ramdisk software
-mkdir -p /mnt/ramdisk   #make the mount directory
-MOUNTCODE="tmpfs       /mnt/ramdisk tmpfs   nodev,nosuid,noexec,nodiratime,size=11M   0 0"
-#check if fstab already has this line. if not add it.
-grep -q -F "$MOUNTCODE" /etc/fstab || echo "$MOUNTCODE" >> /etc/fstab
-mount -a               # mount the ramdisk
+if [ FLAG_RAMDISK == 1 ]; then
+  echo -e "\nSetting up ramdisk for pictures"
+  apt install tmpfs -y    #install the ramdisk software
+  mkdir -p /mnt/ramdisk   #make the mount directory
+  MOUNTCODE="tmpfs       /mnt/ramdisk tmpfs   nodev,nosuid,noexec,nodiratime,size=11M   0 0"
+  #check if fstab already has this line. if not add it.
+  grep -q -F "$MOUNTCODE" /etc/fstab || echo "$MOUNTCODE" >> /etc/fstab
+  mount -a               # mount the ramdisk
+fi
 
 echo -e "\nDone"
