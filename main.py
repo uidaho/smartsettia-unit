52b8f3a9ce3c341f4d330f2787e5fc3459120584
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-13 23:06:11 -0700

    get serial number

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import sensors #sensors.py
 import webcam
 
 
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "NOTFOUND00000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
 
 def job_heartbeat():
     print("I'm working...")
@@ -23,6 +37,17 @@ schedule.every(20).seconds.do(job_heartbeat)
 schedule.every(5).seconds.do(job_sensors)
 #schedule.every(1).seconds.do(job_webcam)
 
+# Global Vars
+SN = NOTSET000000000
+
+#function Deff
+
+def initialize():
+    SN = getserial()
+
+
+#Program start
+initialize()
 while True:
     schedule.run_pending()
     time.sleep(0.1)
