690786d8642ea47b2efedb32c4b845d46f903dff
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-14 00:20:21 -0700

    More rearrangements

--- main.py
+++ main.py
@@ -4,23 +4,6 @@ import time
 import sensors #sensors.py
 import webcam
 
-
-def getserial():
-  # Extract serial from cpuinfo file
-  #Example: 000000000000000d
-  cpuserial = "NOTFOUND_0000000"
-  try:
-    f = open('/proc/cpuinfo','r')
-    for line in f:
-      if line[0:6]=='Serial':
-        cpuserial = line[10:26]
-    f.close()
-  except:
-    cpuserial = "ERROR_0000000000"
-
-  return cpuserial
-
-
 def job_heartbeat():
     print("I'm working...")
 
@@ -36,11 +19,31 @@ def job_webcam():
 schedule.every(20).seconds.do(job_heartbeat)
 schedule.every(5).seconds.do(job_sensors)
 #schedule.every(1).seconds.do(job_webcam)
+#communicate with webserver - receive
+#communicate with webserver - send
+#garage door monitor
+#webserver
 
 # Global Vars
 SN = "NOTSET_000000000"
 
+
 #function Deff
+
+def getserial():
+  # Extract serial from cpuinfo file
+  #Example: 000000000000000d
+  cpuserial = "NOTFOUND_0000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR_0000000000"
+  return cpuserial
+
 
 def initialize():
     SN = getserial()
@@ -49,7 +52,7 @@ def initialize():
 #Program start
 print "Welcome to Smartsettia!"
 initialize()
-print SN
+print "SN=" + SN
 while True:
     schedule.run_pending()
     time.sleep(0.1)
