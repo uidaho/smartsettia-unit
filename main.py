f46cbe9674e55b539cadab3dcafe2f1c6600bd7a
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-16 20:20:00 -0700

    Removed uppercased vars

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def job_heartbeat():
 def job_sensors():
     print("Getting Sensors..")
     sensors.update()
-    print "\ttemp: %d, sysTemp: %d" %(my_globals.sensor_dat["Temperature"], my_globals.sensor_dat["SysTemp"])
+    print "\ttemp: %d, cpu_temp: %d" %(my_globals.sensor_dat["Temperature"], my_globals.sensor_dat["cpu_temp"])
 
 def job_webcam():
     webcam.get_Picture()
--- my_globals.py
+++ my_globals.py
@@ -1,36 +1,36 @@
 # This file contains the global variables used in smartsettia
 
-sensor_dat =    {"Capture_Time":"YYYY-MM-DD HH:MM:SS",
-                "Temperature":-1,
-                "Humidity":-1,
-                "Light_out":-1,                  # ambiant light sensor outside
-                "Light_in":-1,                   # ambiant light sensor inside
-                "Limitsw_open":-1,               # limit switch on open side
-                "Limitsw_close":-1,              # limit switch on close side
-                "Hygrometer_count":0,           # The number of Hygrometers attached
+sensor_dat =    {"capture_time":"YYYY-MM-DD HH:MM:SS",
+                "temperature":-1,
+                "humidity":-1,
+                "light_out":-1,                  # ambiant light sensor outside
+                "light_in":-1,                   # ambiant light sensor inside
+                "limitsw_open":-1,               # limit switch on open side
+                "limitsw_close":-1,              # limit switch on close side
+                "hygrometer_count":0,           # The number of Hygrometers attached
                                                 # sublist of dynamic hygrometers?
-                "Cover_State":"Uninitialized",  # Current state of the cover
-                "SysTemp":-1}                    # pi system temperature
+                "cover_state":"Uninitialized",  # Current state of the cover
+                "cpu_temp":-1}                   # pi system temperature
 
-settings =      {"Name":"UnNamed",                  # Name of Device
+settings =      {"name":"UnNamed",                  # Name of Device
                 "SN":"NOTSET_000000000",            # Pi Serial Number
-                "Server_addr": "https://smartsettia.com/api",
-                "Token": "some token data",         # post token key
-                "Job_sensors_sec":5,                # job runs every x seconds
+                "server_addr": "https://smartsettia.com/api",
+                "token": "some token data",         # post token key
+                "job_sensors_sec":5,                # job runs every x seconds
                 "job_webcam_sec":2,                 # job runs every x seconds
                                                     # other jobs
-                "Cover_schedual_en":1,              # auto schedule enabled
-                "Cover_schedual_open": "some time data",  # open time
-                "Cover_schedual_close": "some time data",}# close time
+                "cover_schedual_en":1,              # auto schedule enabled
+                "cover_time_open": "some time data",  # open time
+                "cover_time_close": "some time data",}# close time
 
 
 # This should move to the server file when created
 server_send_packet =    {"Time":"YYYY-MM-DD HH:MM:SS",
-                        "Sensors": sensor_dat,
-                        "PacketNo":0,
-                        "Token": settings["Token"],
-                        "WebcamPicture":"Binary"}
+                        "sensors": sensor_dat,
+                        "packetNo":0,
+                        "token": settings["Token"],
+                        "webcam_picture":"Binary"}
 
 # not complete
 server_recv_packet =    {"commands": "commands kwarg?",
-                        "New Settings": "new settings kwarg"}
+                        "new_settings": "new settings kwarg"}
