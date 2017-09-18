9f8cd474855155b9f0613366f4ca6800d548af16
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-18 16:12:32 -0700

    Added some webcam notes

--- webcam.py
+++ webcam.py
@@ -1,3 +1,7 @@
+# cv compare: https://stackoverflow.com/questions/11094481/capturing-a-single-image-from-my-webcam-in-java-or-python
+# cv settings: https://stackoverflow.com/questions/32943227/python-opencv-capture-images-from-webcam
+# another compareg: https://softwarerecs.stackexchange.com/questions/18134/python-library-for-taking-camera-images
+# Timestamp: http://startgrid.blogspot.com/2012/08/tutorial-creating-timestamp-on.html
 import time
 
 def get_Picture():
