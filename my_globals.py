ce1bb256f967ada60af4ec3b9c567b38edb8758c
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-09-19 16:16:29 -0700

    temp

--- my_globals.py
+++ my_globals.py
@@ -2,28 +2,37 @@
 
 version= "0.0.1"
 
+# Note to concatonate dictionaries
+# z = x.copy()
+# z.update(y)
+
 sensor_dat =    {"capture_time":"YYYY-MM-DD HH:MM:SS",
+                "light_in":-1,                      # ambiant light sensor inside
+                "light_out":-1,                     # ambiant light sensor outside
+                "limitsw_open":-1,                  # limit switch on open side
+                "limitsw_close":-1,                 # limit switch on close side
+                "cover_state":"Uninitialized",      # Current state of the cover
+                "cpu_temp":-1,                      # pi system temperature
                 "temperature":-1,
                 "humidity":-1,
-                "light_out":-1,                             # ambiant light sensor outside
-                "light_in":-1,                              # ambiant light sensor inside
-                "limitsw_open":-1,                          # limit switch on open side
-                "limitsw_close":-1,                         # limit switch on close side
-                "hygrometer_count":0,                       # The number of Hygrometers attached
-                                                            # sublist of dynamic hygrometers?
+                "hygrometer_count":0,               # The number of Hygrometers attached
+                                    # sublist of dynamic hygrometers?
                 "cover_state":"Uninitialized",              # Current state of the cover
-                "cpu_temp":-1                               # pi system temperature
                 }
 
 settings =      {"name":"UnNamed",                          # Name of Device
-                "uuid": "aa2c0776-9b44-13e7-abc4-cec278b6b50a",   # UUID V1
-                "mac_address":"00:00:00:00:00:00",           # MAC address
+                "uuid": "NOT_SET0-0000-0000-0000-000000000000",   # UUID V1
+                "token": "aSDf7986a89s7df87asd98f7dd",      # post token key
                 "challenge": "temppass",                    # challenge
+                "mac_address":"00:00:00:00:00:00",          # MAC address
                 "server_addr": "https://smartsettia.com/api/ping",
                 "server_reg_addr": "http://smartsettia-backburn.c9users.io/api/register",
-                "token": "aSDf7986a89s7df87asd98f7dd",      # post token key
-                "job_sensors_sec":5,                        # job runs every x seconds
-                "job_webcam_sec":2,                         # job runs every x seconds
+                "job_cover_monitor":1,                      # cover monitor run rate
+                "job_sensors_sec"  :5,                      # job runs every x seconds
+                "job_webcam_sec"   :2,                      # job runs every x seconds
+                "job_server_webcam_sec" :5,                 # send webcam picture job
+                "job_server_status_sec" :1,                 # send device status job
+                "job_server_sensors_sec":20,                # send device sensors job
                                                             # other jobs
                 "cover_schedual_en":1,                      # auto schedule enabled
                 "cover_time_open": "YYYY-MM-DD HH:MM:SS",   # open time
@@ -32,14 +41,36 @@ settings =      {"name":"UnNamed",                          # Name of Device
                 }
 
 
+server_base_packet=    {"time":"YYYY-MM-DD HH:MM:SS",      # time of packet
+                        "uuid": settings["uuid"],
+                        "token": settings["token"],
+                        "version": version,                 # program version
+                        "packetNo":0,                       # packet number
+                        }
+
 # This should move to the server file when created
-server_send_packet =    {"Time":"YYYY-MM-DD HH:MM:SS",      # time of packet
-                        "UUID": settings["uuid"],
+server_send_packet =    {"time":"YYYY-MM-DD HH:MM:SS",      # time of packet
+                        "uuid": settings["uuid"],
+                        "token": settings["token"],
+                        "version": version,                 # program version
+                        "packetNo":0,                       # packet number
+                        }
+
+server_status_packet = {"time":"YYYY-MM-DD HH:MM:SS",      # time of packet
+                        "uuid": settings["uuid"],
+                        "token": settings["token"],
+                        "version": version,                 # program version
+                        "packetNo":0,                       # packet number
+                        "cover_status": "open",
+                        "error_msg": "none"
+                        }
+
+server_webcam_packet = {"time":"YYYY-MM-DD HH:MM:SS",      # time of packet
+                        "uuid": settings["uuid"],
                         "version": version,                 # program version
                         "packetNo":0,                       # packet number
                         "token": settings["token"],
-                        "sensors": sensor_dat,              # sensor data
-                        "webcam_picture":"Binary"          # webcam picture binary
+                        "webcam_picture": "binary"
                         }
 
 # not complete
