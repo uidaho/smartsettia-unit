c16761fdf10caa3baa8f8199d6a8911df86a3e79
Nick <kren5141@vandals.uidaho.edu>
2017-11-01 20:23:16 +0000

    Fixed cpu_temp string conversion

--- sensors.py
+++ sensors.py
@@ -12,11 +12,11 @@ sim_cpuTemp = 35      # needs to be global to simulate Sensors
 # update all sensor values
 def update():
     my_globals.sensor_data["sensor_data"][0]["value"] = str(get_cpu_temp())
-    my_globals.sensor_data["sensor_data"][1]["value"] = str(get_light())
+    my_globals.sensor_data["sensor_data"][1]["value"] = get_light()
     my_globals.sensor_data["sensor_data"][2]["value"] = str(get_light())  # TODO add light inner
     my_globals.sensor_data["sensor_data"][3]["value"] = str(get_Temp_Hum())
     print("Tempsensor [3]: ", my_globals.sensor_data["sensor_data"][3])
-    print ("Temp: %d\tCpu Temp: %r\tLight: %d" % (get_Temp_Hum(), get_cpu_temp(), get_light()))
+    print ("Temp: %s\tCpu Temp: %s\tLight: %s" % (get_Temp_Hum(), get_cpu_temp(), get_light()))
 
 # get temperature and humidity from sensor
 def get_Temp_Hum():
@@ -34,7 +34,7 @@ def get_light():
 def get_cpu_temp():
     "Returns the CPU temerature based on architecture"
     if platform.machine() == "armv7l":
-        return check_output(["/opt/vc/bin/vcgencmd measure_temp | cut -c6-9"], shell=True)[:-1]
+        return check_output(["/opt/vc/bin/vcgencmd measure_temp | cut -c6-9"], shell=True)[:-1].decode('utf-8')
     else:                   # either faking sensors and/or no armv7l architecture detected
         global sim_cpuTemp
         sim_cpuTemp = (sim_cpuTemp + 1) % 50 + 50
